b71fc4e6556df2563f673933e05bdf7968d5f94c
zimin <jianming.zeng@directdrivetech.com>
2023-08-08 16:51:27 +0800

    feat: add can dts settings
    Signed-off-by: zimin <jianming.zeng@directdrivetech.com>

---
+++ applications/Light_leg_ws/BSP/Inc/api_can.h
@@ -0,0 +1,15 @@
+// #ifndef __API_CAN_H
+// #define __API_CAN_H
+
+// #ifdef __cplusplus
+// extern "C" {
+// #endif
+
+// int api_canfd_init();
+// int c_light_canfd_send();
+
+// #ifdef __cplusplus
+// }
+// #endif
+
+// #endif //__API_CAN_H
